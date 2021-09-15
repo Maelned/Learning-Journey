3b23680286448ca167059341805175b934f64a38
Maelned <44137206+Maelned@users.noreply.github.com>
2021-09-15 11:43:03 +0200

    Update Learning journey

---
+++ R/test.R
@@ -0,0 +1,36 @@
+a <- 2
+b <- 3
+c <- 4
+d <- a
+
+
+a = T
+
+
+mode(a)
+is.null(a)
+is.logical(a)
+is.numeric(a)
+is.complex(a)
+is.character(a)
+
+as.logical(a)
+b = as.numeric(a)
+as.complex(a)
+as.character(a)
+
+list <- c(1,3,4)
+test <- c(2,4,5)
+
+
+rm(test)
+
+list
+
+nom <- paste(rep("ind",7),1:3,sep='.')
+nom
+
+x <- c(-1,0,2)
+test <- x>1
+all(x>1)
+
